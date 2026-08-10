5b3c046e6b50725d92478ea9b26a56cb08cda581
Away <damianmischitz12@gmail.com>
2026-08-10 17:53:33 +0200

    Restore grappling listener text imports

--- src/main/java/de/away/mentalheroes/GrapplingHookCraftingListener.java
+++ src/main/java/de/away/mentalheroes/GrapplingHookCraftingListener.java
@@ -1,5 +1,7 @@
 package de.away.mentalheroes;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
